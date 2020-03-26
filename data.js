7c1e7bedb75686992a8c5df526c13ca792d198ec
Frog <shmily40686@gmail.com>
2020-03-26 12:36:01 -0700

    tree done

--- data.js
+++ data.js
@@ -10,3 +10,284 @@ const data = [
     { name:'Oracle', number:136000 },
     { name:'Tesla', number:45000 },
 ];
+
+const data2 = {
+    name: "Alphabet",
+    value: 118899,
+    children: [
+        {
+            name: "Google",
+            value: 114096,
+            children: [
+                {
+                    name: 'YouTube',
+                    value: 1121
+                },
+                {
+                    name: 'DoubleClick',
+                    value: 1500
+                },
+                {
+                    name: 'Motorola Mobility',
+                    value: 17000
+                },
+                {
+                    name: 'Fitbit',
+                    value: 1694
+                },
+                {
+                    name: 'Pointy',
+                    value: 85
+                },
+                {
+                    name: 'Appsheet',
+                    value: 25
+                },
+                {
+                    name: 'Typhoon Studios',
+                    value: 27
+                },
+                {
+                    name: 'CloudSimple',
+                    value: 72
+                },
+                {
+                    name: 'Socratic',
+                    value: 30
+                },
+                {
+                    name: 'Elastifile',
+                    value: 51
+                },
+                {
+                    name: 'Looker',
+                    value: 500
+                },
+                {
+                    name: 'Nightcorn',
+                    value: 46
+                },
+                {
+                    name: 'Alooma',
+                    value: 75
+                },
+                {
+                    name: 'Superpod',
+                    value: 312
+                },
+                {
+                    name: 'Sigmoid Labs',
+                    value: 181
+                },
+                {
+                    name: 'Onward',
+                    value: 2000
+                },
+                {
+                    name: 'Senosis',
+                    value: 118
+                },
+                {
+                    name: 'GraphicsFuzz',
+                    value: 6
+                },
+                {
+                    name: 'Cask',
+                    value: 191
+                },
+                {
+                    name: 'Velostrata',
+                    value: 30
+                },
+                {
+                    name: 'Tenor',
+                    value: 43
+                },
+                {
+                    name: 'Redux',
+                    value: 24
+                },
+                {
+                    name: '60db',
+                    value: 41
+                },
+                {
+                    name: 'Relay Media',
+                    value: 31
+                },
+                {
+                    name: 'Bitium',
+                    value: 16
+                },
+                {
+                    name: 'HTC',
+                    value: 29
+                },
+                {
+                    name: 'AllMatter',
+                    value: 28
+                },
+                {
+                    name: 'Halli Labs',
+                    value: 82
+                },
+                {
+                    name: 'Owlchemy Labs',
+                    value: 23
+                },
+                {
+                    name: 'Limes Audio',
+                    value: 647
+                },
+                {
+                    name: 'Cronologics',
+                    value: 32
+                },
+                {
+                    name: 'Qwiklabs',
+                    value: 85
+                },
+                {
+                    name: 'LeapDroid',
+                    value: 48
+                },
+                {
+                    name: 'FameBit',
+                    value: 46
+                },
+                {
+                    name: 'API.AI',
+                    value: 52
+                },
+                {
+                    name: 'Urban Engineers',
+                    value: 300
+                },
+                {
+                    name: 'Apigee',
+                    value: 374
+                },
+                {
+                    name: 'Orbitera',
+                    value: 505
+                },
+                {
+                    name: 'Kifi',
+                    value: 13
+                },
+                {
+                    name: 'Anvato',
+                    value: 20
+                },
+                {
+                    name: 'Moodstocks',
+                    value: 38
+                },
+                {
+                    name: 'Webpass',
+                    value: 20000
+                },
+                {
+                    name: 'Synergyse',
+                    value: 471
+                },
+                {
+                    name: 'BandPage',
+                    value: 187
+                },
+                {
+                    name: 'Jibe Mobile',
+                    value: 41
+                },
+                {
+                    name: 'Oyster',
+                    value: 42
+                },
+                {
+                    name: 'Thrive Audio',
+                    value: 209
+                },
+                {
+                    value: 32,
+                    name: 'Red Hot Labs'
+                },
+                {
+                    value: 219,
+                    name: 'Firebase'
+                },
+                {
+                    value: 18,
+                    name: 'GreenThrottle'
+                },
+                {
+                    value: 73,
+                    name: 'Impermium'
+                },
+                {
+                    value: 1100,
+                    name: 'Nest Labs'
+                },
+                {
+                    value: 43,
+                    name: 'Autofuss'
+                },
+                {
+                    value: 236,
+                    name: 'Zagat'
+                },
+                {
+                    value: 69,
+                    name: 'DailyDeal'
+                },
+            ],
+        },
+        {
+            name: "Calico",
+            value: 37
+        },
+        {
+            name: "DeepMind",
+            value: 1000
+        },
+        {
+            name: "GV",
+            value: 200
+        },
+        {
+            name: "CapitalG",
+            value: 134
+        },
+        {
+            name: "X",
+            value: 626
+        },
+        {
+            name: "Google Fiber",
+            value: 60
+        },
+        {
+            name: "Makani",
+            value: 20
+        },
+        {
+            name: "Sidewalk Labs",
+            value: 129
+        },
+        {
+            name: "Verily",
+            value: 235
+        },
+        {
+            name: "Waymo",
+            value: 950
+        },
+        {
+            name: "Wing",
+            value: 49
+        },
+        {
+            name: "Loon",
+            value: 186
+        }
+    ]
+}
